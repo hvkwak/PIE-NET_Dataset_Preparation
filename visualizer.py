00b990a954f818422f27a71b2b445aa5679ab23c
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2021-12-08 18:42:37 +0100

    minor changes.

--- visualizer.py
+++ visualizer.py
@@ -4,23 +4,58 @@ import scipy.io as sio
 from scipy.interpolate import splprep, splev
 
 def main():
-    '''
-    ref_mat = sio.loadmat('/home/pro2future/Documents/PIE-NET_Dataset_Preparation/5.mat')['Training_data']    
-    for i in range(30, 40):
+    
+    ref_mat = sio.loadmat('/home/pro2future/Documents/PIE-NET_Dataset_Preparation/5.mat')['Training_data']
+    my_mat = sio.loadmat('/home/pro2future/Documents/PIE-NET_Dataset_Preparation/0006_0.mat')['Training_data']
+    
+    ref_mat_down_sample_point_max = 0.0
+    ref_mat_down_sample_point_min = 0.0
+    my_mat_down_sample_point_max = 0.0
+    my_mat_down_sample_point_min = 0.0
+
+    for i in range(0, 64):
+        ref_down_sample_point = ref_mat[i, 0]['down_sample_point'][0, 0]
+        ref_PC_8096_edge_points_label_bin = np.where(ref_mat[i, 0]['PC_8096_edge_points_label_bin'][0, 0][:, 0] == 1)[0]
+        ref_corner_points_label_bin = np.where(ref_mat[i, 0]['corner_points_label'][0, 0][:, 0] == 1)[0]
+        down_sample_point = my_mat[i, 0]['down_sample_point'][0, 0]
+        edge_points_label = np.where(my_mat[i, 0]['edge_points_label'][0, 0][0,:] == 1)[0]
+        corner_points_label = np.where(my_mat[i, 0]['corner_points_label'][0, 0][0,:] == 1)[0]
+
+        #print("max", i, "X: ", np.max(ref_down_sample_point[:, 0]))
+        #print("max", i, "Y: ", np.max(ref_down_sample_point[:, 1]))
+        #print("max", i, "Z: ", np.max(ref_down_sample_point[:, 2]))
+        #print("min", i, " ", np.min(ref_down_sample_point))
+        ref_mat_down_sample_point_max += np.max(ref_down_sample_point)
+        ref_mat_down_sample_point_min += np.min(ref_down_sample_point)
+        my_mat_down_sample_point_max += np.max(down_sample_point)
+        my_mat_down_sample_point_min += np.min(down_sample_point)
+
+    ref_mat_down_sample_point_max = ref_mat_down_sample_point_max / 64.0
+    ref_mat_down_sample_point_min = ref_mat_down_sample_point_min / 64.0
+    my_mat_down_sample_point_max = my_mat_down_sample_point_max / 64.0
+    my_mat_down_sample_point_min = my_mat_down_sample_point_min / 64.0
+
+    print("****** BASIC STATISTICS ******" )
+    print("ref down_sample_point in [", ref_mat_down_sample_point_min, ", ", ref_mat_down_sample_point_max, "]")
+    print("my down_sample_point in [", my_mat_down_sample_point_min, ", ", my_mat_down_sample_point_max, "]")
+
+        
+    for i in range(0, 64):
         ref_down_sample_point = ref_mat[i, 0]['down_sample_point'][0, 0]
         ref_PC_8096_edge_points_label_bin = np.where(ref_mat[i, 0]['PC_8096_edge_points_label_bin'][0, 0][:, 0] == 1)[0]
         ref_corner_points_label_bin = np.where(ref_mat[i, 0]['corner_points_label'][0, 0][:, 0] == 1)[0]
         view_point_1(ref_down_sample_point, ref_PC_8096_edge_points_label_bin, ref_corner_points_label_bin)
-    '''
-
-    my_mat = sio.loadmat('/home/pro2future/Documents/PIE-NET_Dataset_Preparation/0015_0.mat')['Training_data']
-    for i in range(0, 64):
+    
         down_sample_point = my_mat[i, 0]['down_sample_point'][0, 0]
         edge_points_label = np.where(my_mat[i, 0]['edge_points_label'][0, 0][0,:] == 1)[0]
         corner_points_label = np.where(my_mat[i, 0]['corner_points_label'][0, 0][0,:] == 1)[0]
-        #view_point_1(down_sample_point, edge_points_label, corner_points_label)
-        print("corner_points_label: ", corner_points_label.shape)
+        view_point_1(down_sample_point, edge_points_label, corner_points_label)
+        #print("corner_points_label: ", corner_points_label.shape)
     
+
+
+
+
     '''
     from scipy.interpolate import splprep, splev
     x1 = np.array([0, 0, 0], dtype = np.float64)
